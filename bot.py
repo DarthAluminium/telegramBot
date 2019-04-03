25f6dd78ee2b67d2f7d2536cb4a1f2db8d55a2ae
DarthAluminium <43782012+DarthAluminium@users.noreply.github.com>
2019-04-03 18:23:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import requests
+'''import requests
 import telebot
 import re
 import token
@@ -51,4 +51,56 @@ def format_hw_msg_all(msg):
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?"
 if name == 'main':
+    bot.polling(none_stop=True)'''
+import requests
+import datetime
+import conf
+import telebot
+
+class Msk(datetime.tzinfo):
+    def utcoffset(self, dt):
+      return datetime.timedelta(hours=+3)
+
+    def dst(self, dt):
+        return datetime.timedelta(0)
+
+bot = telebot.TeleBot(conf.token)
+
+def sleep(h, m, s):
+    try:
+        hour_start = datetime.datetime.now(Msk()).hour
+        minute_start = datetime.datetime.now(Msk()).minute
+        second_start = datetime.datetime.now(Msk()).second
+        hour_stop = 0
+        minute_stop = 0
+        second_stop = 0
+        while((hour_start+int(h) != hour_stop) or int(h) == 0):
+            while(minute_start+int(m) != minute_stop or int(m) == 0):
+                while(int(second_start+int(s)) != int(second_stop)):
+                    hour_stop = datetime.datetime.now(Msk()).hour
+                    minute_stop = datetime.datetime.now(Msk()).minute
+                    second_stop = datetime.datetime.now(Msk()).second
+                if int(m) == 0:
+                    break
+            if int(h) == 0:
+                break
+        return 'Stop'
+    except TypeError:
+        return 'Error'
+        
+
+
+
+@bot.message_handler(content_types=["text"])
+def timer(message):
+    bot.send_message(message.chat.id, 'Start')
+    try:
+        time = message.text.split()
+        bot.send_message(message.chat.id, sleep(time[0], time[1], time[2]))
+    except IndexError:
+        bot.send_message(message.chat.id, 'Это самодельный таймер, бот вопринимает только строки в формате h m s, где h это часы m минуты, а s секунды')
+    
+
+
+if __name__ == '__main__':
     bot.polling(none_stop=True)
